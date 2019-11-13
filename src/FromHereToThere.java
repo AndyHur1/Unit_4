9d982800db01e1c06cafa5c37d81ff8a0376bb09
AndyHur1 <hurby@ssfs.org>
2019-11-13 14:38:38 -0500

    fromheretothere

--- src/FromHereToThere.java
+++ src/FromHereToThere.java
@@ -1,3 +1,17 @@
 public class FromHereToThere {
-    public static String 
+    public static String fromHereToThere (int a, int b){
+        String output= " ";
+        if (a>b) {
+            return "Invalid input";
+        }else
+            while(a<b){
+                ++a;
+            output+= a+" ";}
+        return output;
+
+
+    }
+    public static void main(String[] args){
+        System.out.println(fromHereToThere(20,30));
+    }
 }
