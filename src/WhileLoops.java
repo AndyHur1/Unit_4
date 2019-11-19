fceefa274237befc737b2f4717ad6b292672bdd4
AndyHur1 <hurby@ssfs.org>
2019-11-19 11:20:20 -0500

    Still working on gradepoint

--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
 import java.util.Scanner;
 
-public class FromHereToThere {
+
+public class WhileLoops {
+    static Scanner scan =new Scanner(System.in);
     public static String fromHereToThere (int a, int b){
         String output= " ";
         if (a>b) {
@@ -22,13 +24,12 @@ public class FromHereToThere {
                 output += y + " ";
             ++y;
         }
-        return output;
+        return "The factors of "+a+" are:"+output;
     }
     public static String countPosAndNeg(){
         int negative_num=0;
         int positive_num=0;
         int f_num=1;
-        Scanner scan = new Scanner(System.in);
 
         while (f_num!=0) {
             System.out.println("Enter a positive or negative number or 0 to quit:");
@@ -47,7 +48,6 @@ public class FromHereToThere {
         int max_value = Integer.MIN_VALUE;
         int min_value = Integer.MAX_VALUE;
         while(num<5){
-            Scanner scan =new Scanner(System.in);
             System.out.print("Number: ");
             int num_1 = scan.nextInt();
             if (num_1>max_value)
@@ -56,37 +56,36 @@ public class FromHereToThere {
                 min_value=num_1;
             num++;
         }
-        return "Max Value is: "+max_value+"\n"+"Min Value is: "+min_value;
+        return "Max value is: "+max_value+"\n"+"Min value is: "+min_value;
     }
-    public static String gradePoint(){
+    public static double gradePoint() {
         int num = 0;
         double grade_num = 0;
 
-        Scanner scan = new Scanner(System.in);
         System.out.println("Enter seven letter grades (A, B, C, D, or F)");
-        while(num<7){
+        while (num < 7) {
             String grade = scan.next();
-            if (grade=="A")
-                grade_num+=4.0;
-            else if (grade=="B")
-                grade_num+=3.0;
-            else if (grade=="C")
-                grade_num+=2.0;
-            else if (grade=="D")
-                grade_num+=1.0;
-            else if (grade=="F")
-                grade_num+=0.0;
+            if (grade.equals("A"))
+                grade_num += 4.0;
+            else if (grade.equals("B"))
+                grade_num += 3.0;
+            else if (grade.equals("C"))
+                grade_num += 2.0;
+            else if (grade.equals("D"))
+                grade_num += 1.0;
+            else if (grade.equals("F"))
+                grade_num += 0.0;
             else
                 System.out.println("Invalid grade");
+            num++;
         }
-        double avg_grade = grade_num/7;
-        return "GPA = "+avg_grade;
+        double avg_grade = grade_num / 7;
+        double final_gpa= (int) ((avg_grade * 100) + 0.5);
+        return final_gpa/100;
+
+
     }
-
-
-
-
     public static void main(String[] args){
-        System.out.println(gradePoint());
+        System.out.println(factors(17));
     }
 }
