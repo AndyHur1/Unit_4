4dfca2f702b81799ce56da02b3f43d7194f70793
AndyHur1 <hurby@ssfs.org>
2019-12-04 10:10:23 -0500

    still working on mult

--- src/NestedLoops.java
+++ src/NestedLoops.java
@@ -12,8 +12,53 @@ public class NestedLoops {
         }
         return x;
     }
+    public static String xSquare2(int k){
+
+        String x = "";
+        for (int a=0;a<k;a++){
+            for (int b=0;b<k;b++){
+                x+="X";
+            }
+            x+="\n";
+        }
+        return x;
+
+    }
+    public static String flippedTriangle(int n){
+
+        String x = "";
+        for (int a=0;a<n;a++){
+
+            for (int b=a;b<n;b++){
+                x+="*";
+            }
+            x+="\n";
+        }
+        return x;
+
+
+    }
+    public static String multiplicationTable(){
+
+        String k = "";
+
+        for (int a=1;a<=9;a++){
+            for (int b=1;b<=9;b++){
+                if (a*b<10){
+                    k+=" "+a*b+" ";
+                }else{
+                    k+=a*b+" ";
+                }
+
+            }
+
+            k+="\n";
+
+        }
+        return k;
+    }
     public static void main(String[] args){
 
-        System.out.println(xSquare());
+        System.out.println(multiplicationTable());
     }
 }
